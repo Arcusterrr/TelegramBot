83099aff63e7835e646f3e2b20873407b38c31e0
Arcusterrr <nikita.kargapolov000@gmail.com>
2022-08-30 13:07:35 +0500

    start

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 
 import config
 
@@ -7,8 +8,16 @@ bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
+    bot.send_message(message.chat.id, f'Привет ✌, {message.from_user.first_name}!\nПредлагаю тебе зарегистрироваться!\nСкорее нажимай: /register')
+    # markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    # item1 = types.KeyboardButton("Зарегистрироваться")
+    # markup.add(item1)
 
-    # bot.send_message(message.chat.id, f'Привет ✌, {message.from_user.first_name}!')
-
+@bot.message_handler(commands=['register'])
+def button_message(message):
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1=types.KeyboardButton("Регистрация")
+    markup.add(item1)
+    bot.send_message(message.chat.id,'Выберите что вам надо',reply_markup=markup)
 
 bot.infinity_polling()
